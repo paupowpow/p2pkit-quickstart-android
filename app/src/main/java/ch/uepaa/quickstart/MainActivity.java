e1bb1b39274aebd4205a70f31986668c24897190
David Bengtsson <david.bengtsson@uepaa.ch>
2015-04-15 16:52:31 +0200

    Formatted code, added strings to resources

--- app/src/main/java/ch/uepaa/quickstart/MainActivity.java
+++ app/src/main/java/ch/uepaa/quickstart/MainActivity.java
@@ -1,9 +1,8 @@
 package ch.uepaa.quickstart;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.UUID;
@@ -18,14 +17,73 @@ import ch.uepaa.p2pkit.discovery.P2pListener;
 import ch.uepaa.p2pkit.messaging.MessageListener;
 import ch.uepaa.p2pkit.messaging.MessageServices;
 
-
 public class MainActivity extends ActionBarActivity {
 
     private static final String APP_KEY = "<YOUR APP KEY>";
+    private final P2pListener mP2pDiscoveryListener = new P2pListener() {
+        @Override
+        public void onPeerDiscovered(final UUID nodeId) {
+            logToView("P2pListener | Peer discovered: " + nodeId);
 
+            // sending a message to the peer
+            KitClient.getMessageService().sendMessage(nodeId, "SimpleChatMessage", "From Android: Hello P2P!".getBytes());
+        }
+
+        @Override
+        public void onPeerLost(final UUID nodeId) {
+            logToView("P2pListener | Peer lost: " + nodeId);
+        }
+    };
+    private final GeoListener mGeoDiscoveryListener = new GeoListener() {
+        @Override
+        public void onStateChanged(int state) {
+            logToView("GeoListener | State changed: " + state);
+        }
+
+        @Override
+        public void onPeerDiscovered(final UUID nodeId) {
+            logToView("GeoListener | Peer discovered: " + nodeId);
+
+            // sending a message to the peer
+            KitClient.getMessageService().sendMessage(nodeId, "SimpleChatMessage", "From Android: Hello GEO!".getBytes());
+        }
+
+        @Override
+        public void onPeerLost(final UUID nodeId) {
+            logToView("GeoListener | Peer lost: " + nodeId);
+        }
+    };
+    private final MessageListener mMessageListener = new MessageListener() {
+        @Override
+        public void onStateChanged(int state) {
+            logToView("MessageListener | State changed: " + state);
+        }
+
+        @Override
+        public void onMessageReceived(long timestamp, UUID origin, String type, byte[] message) {
+            logToView("MessageListener | Message received: From=" + origin + " type=" + type + " message=" + new String(message));
+        }
+    };
     private TextView logView;
-
     private KitClient mP2pClient;
+    private final ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks() {
+        @Override
+        public void onConnected() {
+            logToView("Successfully connected to P2P Services");
+        }
+
+        @Override
+        public void onConnectionSuspended() {
+            logToView("Connection to P2P Services suspended");
+        }
+
+        @Override
+        public void onConnectionFailed(ConnectionResult connectionResult) {
+            logToView("Connection to P2P Services failed with status: " + connectionResult.getStatusCode());
+            ConnectionResultHandling.showAlertDialogForConnectionError(MainActivity.this, connectionResult.getStatusCode());
+            mP2pClient = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +100,11 @@ public class MainActivity extends ActionBarActivity {
         });
 
         findViewById(R.id.stopButton).setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 stopAll();
-             }
-         }
-        );
-
+            @Override
+            public void onClick(View view) {
+                stopAll();
+            }
+        });
     }
 
     private void startAll() {
@@ -77,76 +133,11 @@ public class MainActivity extends ActionBarActivity {
         if (mP2pClient != null) {
             logToView("Stopping everything");
             mP2pClient.disconnect();
+            mP2pClient = null;
         } else {
             logToView("Nothing to stop");
         }
     }
-
-
-    private final ConnectionCallbacks mConnectionCallbacks = new ConnectionCallbacks() {
-        @Override
-        public void onConnected() {
-            logToView("Successfully connected to P2P Services");
-        }
-
-        @Override
-        public void onConnectionSuspended() {
-            logToView("Connection to P2P Services suspended");
-        }
-
-        @Override
-        public void onConnectionFailed(ConnectionResult connectionResult) {
-            logToView("Connection to P2P Services failed with status: " + connectionResult.getStatusCode());
-        }
-    };
-
-    private final P2pListener mP2pDiscoveryListener = new P2pListener() {
-        @Override
-        public void onPeerDiscovered(final UUID nodeId) {
-            logToView("P2pListener | Peer discovered: " + nodeId);
-
-            // sending a message to the peer
-            KitClient.getMessageService().sendMessage(nodeId, "SimpleChatMessage", "From Android: Hello P2P!".getBytes());
-        }
-
-        @Override
-        public void onPeerLost(final UUID nodeId) {
-            logToView("P2pListener | Peer lost: " + nodeId);
-        }
-    };
-
-
-    private final GeoListener mGeoDiscoveryListener = new GeoListener() {
-        @Override
-        public void onStateChanged(int state) {
-            logToView("GeoListener | State changed: " + state);
-        }
-
-        @Override
-        public void onPeerDiscovered(final UUID nodeId) {
-            logToView("GeoListener | Peer discovered: " + nodeId);
-
-            // sending a message to the peer
-            KitClient.getMessageService().sendMessage(nodeId, "SimpleChatMessage", "From Android: Hello GEO!".getBytes());
-        }
-
-        @Override
-        public void onPeerLost(final UUID nodeId) {
-            logToView("GeoListener | Peer lost: " + nodeId);
-        }
-    };
-
-    private final MessageListener mMessageListener = new MessageListener() {
-        @Override
-        public void onStateChanged(int state) {
-            logToView("MessageListener | State changed: " + state);
-        }
-
-        @Override
-        public void onMessageReceived(long timestamp, UUID origin, String type, byte[] message) {
-            logToView("MessageListener | Message received: From=" + origin + " type=" + type + " message=" + new String(message));
-        }
-    };
 
     private void logToView(String message) {
         logView.setText(message + "\n" + logView.getText());
